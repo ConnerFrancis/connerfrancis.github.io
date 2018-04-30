5f57c5152b9ed241e091bd5ad0197efa1dbac293
ConnerFrancis <thxbro79@gmail.com>
2018-04-30 07:17:41 -0400

    SCSS, mainly.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,48 +1,135 @@
 <template>
 
   <div id="home">
-    <div class="pages">
-      <router-link
-        v-for="page in pages"
-        v-show="((page.name != 'Home') && (page.name != 'HomeOld'))"
-        :key="page.name"
-        :to="page.path"
-      >{{ page.path }}</router-link>
+
+    <div class="top">
+
+      <div class="left">
+        <h1 class="title" @click="genTitle()">conner's<br/>{{ title }}</h1>
+      </div>
+
+      <div class="right">
+
+        <h1 class="title">pages</h1>
+        <div class="pages">
+          <router-link
+            v-for="page in pages"
+            v-show="((page.name != 'Home') && (page.name != 'HomeOld'))"
+            :key="page.name"
+            :to="page.path"
+            ><sotc>{{ page.meta.name }}</sotc></router-link>
+          </div>
+
+        </div>
+
     </div>
+
   </div>
 
 </template>
 
 <script>
   import pages from '@/pages'
+  import mh    from '@/services/mathhelper'
+
+  import SlideOverTextColor from '@/components/SlideOverTextColor'
 
   export default {
     name: 'Home',
-    
+
+    components: {
+      'sotc': SlideOverTextColor
+    },
+
     data () {
       return {
-        pages
+        pages,
+
+        title: 'enabled javascript, dork',
+        lastTitle: '',
+        titles: [
+          'dungeon',
+          'grotto',
+          'cave',
+          'hovel'
+        ]
       }
+    },
+
+    methods: {
+      genTitle () {
+        this.lastTitle = this.title
+        while(this.title == this.lastTitle) {
+          this.title = mh.sample(this.titles)
+        }
+      }
+    },
+
+    created () {
+      this.genTitle()
     }
   }
 
 </script>
 
 <style lang="scss" scoped>
-  
+
   #home {
     @include flex-container;
-    @include flex-part;
+    @include flex-part($padding: 0);
     @include jumbotron;
-    /* Colors */
-    background: linear-gradient($white, $grey-lighter);
-    
-    .pages {
-      @include flex-container($dir: column);
-      @include flex-part;
-      
-      a { text-decoration: none; }
+    font-family: $font-main;
+
+    .top {
+      @include flex-container;
+      @include flex-part($padding: 72px);
+      @include jumbotron;
+      /* Colors */
+      background-color: white;
+
+      .left {
+        @include flex-container($vert: center);
+        @include flex-part($col: 6, $padding: 96px);
+        @include jumbotron;
+        /* Colors */
+        background-color: $grey-darkest;
+        color: $white;
+
+        .title {
+          @include flex-container($dir: column);
+          @include flex-part;
+          /* Font */
+          font-family: $font-main;
+          font-size: 6em;
+          text-align: left;
+          /* Misc */
+          cursor: pointer;
+        }
+      }
+
+      .right {
+        @include flex-container($dir: column);
+        @include flex-part($col: 6, $padding: 0);
+        @include jumbotron;
+        /* Colors */
+        background-color: $white;
+        color: $grey-darkest;
+
+        .title {
+          padding: 0;
+        }
+
+        .pages {
+          @include flex-container($dir: column);
+          @include flex-part($padding: 0);
+          @include jumbotron;
+
+          a {
+            text-decoration: none;
+          }
+        }
+      }
     }
   }
-  
+
 </style>
