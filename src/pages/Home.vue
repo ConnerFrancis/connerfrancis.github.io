2cf4fc7514a66577edcd3d35e290a6605f17b211
kumo <thxbro79@gmail.com>
2018-09-20 22:25:23 +0000

    Redesign work.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,10 +3,11 @@
   <div id="home">
     <div class="left">
       <div class="links">
-        <a class="soft">Example soft link</a>
+        <a class="soft">YouTube</a>
+        <a class="soft">GitHub</a>
         <a class="hard">Example hard link</a>
       </div>
-      <div class="pfp"></div>
+      <div class="pfp"><img src="../assets/global/me.jpg" /></div>
     </div>
     
     <div class="right">
@@ -31,18 +32,51 @@
   #home {
     // Display
     display: flex;
-    justify-content: center;
+    align-self: stretch;
     // Size
-    max-height: 100vh;
-    max-width: 100vw;
+    width: 100%;
+    height: 100vh;
     padding: 96px;
     
     .left {
       // Display
       display: flex;
+      flex-direction: column;
+      justify-content: space-between;
       // Size
       width: 50%;
-      background-color: black;
+      //background-color: black;
+      
+      .links {
+        // Display
+        display: flex;
+        flex-direction: column;
+        // Font
+        font-weight: bold;
+        
+        a {
+          cursor: pointer;
+          margin-bottom: 8px;
+          
+          &.soft {
+            opacity: 0.5;
+          }
+        }
+      }
+      
+      .pfp {
+        // Size
+        height: 192px;
+        width: 192px;
+        // Colors
+        background-color: #ededed;
+        
+        overflow: hidden;
+        
+        img {
+          max-height: 192px;
+        }
+      }
     }
     
     .right {
